68c2bf1e85ba909134521651e22240a2d6c50249
hatim77100 <najoua-hatim@hotmail.fr>
2022-12-30 09:39:29 +0100

    update exo2

--- src/exo2.js
+++ src/exo2.js
@@ -4,7 +4,9 @@ et le retourne trié selon deux clés.
 Le tableau doit être trié d'abord par prix et ensuite par quantité.
 
 Cela signifie que si deux objets ont la même valeur pour la propriété prix, 
-ils sont ensuite triés par quantité.*/
+ils sont ensuite triés par quantité.
+La fonction doit trier les objets par quantité de manière décroissante 
+si plusieurs objets ont le même prix*/
 
 const arr = [
     { prix: 20, quantite: 10 },
@@ -12,11 +14,15 @@ const arr = [
     { prix: 50, quantite: 1 }
 ]
 
-function trierTableau(...arr) {
+function trierTableau(arr) {
     const b = arr.sort((a, b) => {
-        { b.prix - a.prix }; { b.quantite - a.quantite }
-});
-    return b;
+        if (b.prix = a.prix) {
+           b.quantite-a.quantite 
+        } else {
+            b.prix - a.prix
+        }
+    });
+    console.log(b); 
 }
 
-console.log(trierTableau(arr));
+trierTableau(arr);
